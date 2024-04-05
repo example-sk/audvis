cbe16bff1223bbf1444d0cc4d710067a3a502dc6
example.sk <no@email>
2024-04-05 17:31:01 +0200

    5.0.3

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ import sys
 bl_info = {
     "name": "AudVis - audio visualization driver",
     "author": "example.sk",
-    "version": (5, 0, 2),
+    "version": (5, 0, 3),
     "blender": (2, 80, 0),
     "location": "View 3D > Sidebar > AudVis Tab",
     "description": (
