0f74c13a1be2d97cc3aca52615e0008f40114150
example.sk <no@email>
2024-05-10 17:20:36 +0200

    party mode - disable "Window" party mode implementation because of python api changes in Blender 4.?. Only "Workspace" mode remains.

--- ui/props/party.py
+++ ui/props/party.py
@@ -1,5 +1,21 @@
 import bpy
 
+impl_window = (
+    'window',
+    'Window',
+    'Opens temporary window and maximizes it. Use if you want to control Blender from main window.'
+)
+impl_workspace = (
+    'workspace',
+    'Workspace',
+    'Adds a workspace. Use for higher FPS.'
+)
+
+if bpy.app.version[0] < 4:
+    _supported_implementations = [impl_window, impl_workspace]
+else:
+    _supported_implementations = [impl_workspace]
+
 
 class AudvisPartyProperties(bpy.types.PropertyGroup):
     show_xray: bpy.props.BoolProperty(name="Show X-Ray for Wireframe in Party Mode", default=False)
@@ -10,8 +26,4 @@ class AudvisPartyProperties(bpy.types.PropertyGroup):
         ('MATERIAL', 'Material', ''),
         ('RENDERED', 'Rendered', ''),
     ], default='RENDERED')
-    implementation: bpy.props.EnumProperty(name="Implementation of Party Mode", items=[
-        ('window', 'Window',
-         'Opens temporary window and maximizes it. Use if you want to control Blender from main window.'),
-        ('workspace', 'Workspace', 'Adds a workspace. Use for higher FPS.'),
-    ])
+    implementation: bpy.props.EnumProperty(name="Implementation of Party Mode", items=_supported_implementations)
