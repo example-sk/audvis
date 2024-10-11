9c2cb11c18b71a6c1a5e4d9a58fb04f95679f0e3
example.sk <no@email>
2024-10-11 18:18:52 +0200

    install lib - disable if installed as extension

--- ui/install_ui/all.py
+++ ui/install_ui/all.py
@@ -8,6 +8,10 @@ class AUDVIS_OT_InstallAll(bpy.types.Operator):
     bl_label = "Install all libraries for AudVis"
     bl_description = "Install needed python modules"
 
+    @classmethod
+    def poll(cls, context):
+        return not bpy._audvis_module.startswith("bl_ext.")
+
     def execute(self, context):
         install_lib.PipInstaller().install()
         return {'FINISHED'}
